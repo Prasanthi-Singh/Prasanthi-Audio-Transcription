98cab44fa9b806443cd9fb1166e5606b8df35be2
Prasanthi-Singh <118281251+Prasanthi-Singh@users.noreply.github.com>
2024-01-09 14:03:07 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -1,16 +1,15 @@
-from flask import Flask, request , jsonify , make_response , g
+from flask import Flask, request, jsonify, make_response, g
 from flask_restful import Resource, Api, reqparse
 import whisper
 import os
 from functools import wraps
 
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from pydub import AudioSegment
-from database import User , Transcripts
 from collections import Counter
 import re
 import nltk
+
 nltk.download('omw-1.4')
 nltk.download('wordnet')
 from nltk.tokenize import word_tokenize
@@ -18,25 +17,25 @@ from nltk import FreqDist
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import firebase_admin
-from firebase_admin import credentials , auth
+from firebase_admin import credentials, auth, firestore
 from flask_cors import CORS
 from flask_caching import Cache
 
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:prasanthi@localhost:5433/kyro'
-db = SQLAlchemy(app)
 api = Api(app)
+
 CORS(app)
 
+model = whisper.load_model("base")
 
-cred = credentials.Certificate("./prasanthi-kyro-project-firebase-adminsdk-jo6lc-ee73cc4ccb.json")
+cred = credentials.Certificate("./voice-analyser-21f2001040-firebase-adminsdk-77b0d-d4a1954d5f.json")
 firebase_admin.initialize_app(cred)
 
+db = firestore.client()
 
 app.config["CACHE_TYPE"] = "SimpleCache"
 app.config['CACHE_DEFAULT_TIMEOUT'] = 24 * 60 * 60
-cache= Cache(app)
+cache = Cache(app)
 
 # Configuration for file upload
 UPLOAD_FOLDER = 'uploads'
@@ -48,10 +47,10 @@ def authentication_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
+            print(request.headers)
             access_token = dict(request.headers).get("User-Token")
             if not access_token:
                 return make_response({"error_code": "TOKEN100", "error_message": "Id Token Missing"}, 404)
-
             user = auth.verify_id_token(access_token, check_revoked=True)
             g.user = user
             return func(*args, **kwargs)
@@ -64,6 +63,7 @@ def authentication_required(func):
 
     return wrapper
 
+
 # In-memory database to store uploaded files
 uploaded_files = []
 
@@ -71,13 +71,10 @@ uploaded_files = []
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
-
 class FileTranscription(Resource):
     @authentication_required
     def post(self):
-        print('chutiya pro')
         try:
-            print('chutiya')
             # Get the file and uid from the request
             file = request.files['file']
             uid = request.form.get('uid')
@@ -94,7 +91,6 @@ class FileTranscription(Resource):
             print(f"File Name: {file_name}, File Size: {file_size} bytes")
 
             # Load the Whisper ASR model
-            model = whisper.load_model("base")
 
             # Transcribe the audio content of the file
             result = model.transcribe(file_path, task='translate')
@@ -103,10 +99,11 @@ class FileTranscription(Resource):
             transcribed_text = result["text"]
             print(f"Transcribed Text: {transcribed_text}")
 
-            # Store uid and transcribed_text in the database
-            new_transcript = Transcripts(user_id=uid, transcript=transcribed_text)
-            db.session.add(new_transcript)
-            db.session.commit()
+            db.collection('userData').document(g.user.get('uid')).collection('transcriptions').add({
+                'transcribed text': transcribed_text,
+                'created_at': firestore.SERVER_TIMESTAMP
+            })
+
             cache.clear()
 
             return {'message': transcribed_text}, 200
@@ -116,29 +113,34 @@ class FileTranscription(Resource):
 
 
 class TranscriptList(Resource):
-    @cache.memoize(timeout=24*60*60)
-    def get(self, uid):
-        print('yes')
+    @authentication_required
+    def get(self):
         try:
             # Fetch transcripts for the given uid
-            transcripts = db.session.query(Transcripts).filter_by(user_id=uid).all()
-
-
-            # Transform transcripts to a list of dictionaries
-            transcripts_list = [
-                {
-                    'transcript_id': transcript.transcript_id,
-                    'transcript': transcript.transcript,
-                    'created_at': transcript.created_at.strftime('%Y-%m-%d %H:%M:%S')
+            print(g.user)
+            query = db.collection('userData').document(g.user.get('uid'))
+            if query.get().exists:
+                query = query.collection('transcriptions').stream()
+            else:
+                db.collection('userData').document(g.user.get('uid')).set({'created_at':firestore.SERVER_TIMESTAMP})
+                query=[]
+            transcripts_list = [{
+                'id':doc.id,
+                "data":{
+                    'text':doc.to_dict().get('transcribed text')
                 }
-                for transcript in transcripts
-            ]
-
+            } for doc in query]
             return {'transcripts': transcripts_list}, 200
         except Exception as e:
-            print(f"Error fetching transcripts: {str(e)}")
+            print(e)
             return {'error': 'Failed to fetch transcripts'}, 500
-        
+
+class Test(Resource):
+    @authentication_required
+    def get(self):
+        return jsonify(message="HEYYY")
+
+
 def get_transcripts(uid):
     transcripts = db.session.query(Transcripts).filter_by(user_id=uid).all()
     print('1')
@@ -151,38 +153,37 @@ def analyze_text(transcripts):
     all_words = []
     all_phrases = []
 
-# Initialize WordNet Lemmatizer
+    # Initialize WordNet Lemmatizer
     lemmatizer = WordNetLemmatizer()
-    print(all_text , 'yes')
+    print(all_text, 'yes')
     # Tokenize the transcript into words
     words = word_tokenize(all_text.lower())  # Assuming transcript is in the first column
-        # print(words)
-        # Lemmatize words
+    # print(words)
+    # Lemmatize words
     lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
-        
+
     all_words.extend(lemmatized_words)
 
-        # Create phrases (n-grams)
-    phrases = [f"{words[i]} {words[i+1]} {words[i+2]} {words[i+3]} {words[i+4]}" for i in range(len(lemmatized_words)-5)]
+    # Create phrases (n-grams)
+    phrases = [f"{words[i]} {words[i + 1]} {words[i + 2]} {words[i + 3]} {words[i + 4]}" for i in range(len(lemmatized_words) - 5)]
     all_phrases.extend(phrases)
 
     # Remove stop words
     stop_words = set(stopwords.words('english'))
     filtered_words = [word for word in all_words if word.isalnum() and word not in stop_words]
 
-        # Count the frequency of words and phrases
+    # Count the frequency of words and phrases
     word_freq = FreqDist(filtered_words)
     phrase_freq = FreqDist(all_phrases)
 
-        # Get the most frequent 3 words and phrases
+    # Get the most frequent 3 words and phrases
     phrases = []
     most_words = word_freq.most_common(3)
     most_phrases = phrase_freq.most_common(3)
     words = [word for word, _ in most_words]
     phrases = [phrase for phrase, _ in most_phrases]
-    print(words)
-    return words, phrases
 
+    return words, phrases
 
 
 class FrequentWords(Resource):
@@ -202,12 +203,8 @@ class FrequentWords(Resource):
 
 api.add_resource(FrequentWords, '/frequentwords/<user_id>')
 api.add_resource(FileTranscription, '/transcribe')
-api.add_resource(TranscriptList, '/transcripts/<string:uid>')
-
+api.add_resource(TranscriptList, '/transcripts')
+api.add_resource(Test,'/test')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
